ee183730daea6c161cb56856c382cf18bfcb989a
Retetzu <javiera208577@gmail.com>
2022-03-29 20:16:08 -0300

    Clase 2

---
+++ src/Components/NavBar.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        Ink Machine
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-item nav-link active" href="#">
+            Home
+          </a>
+          <a class="nav-item nav-link" href="#">
+            Categories
+          </a>
+          <a class="nav-item nav-link" href="#">
+            Contact
+          </a>
+          <a class="nav-item nav-link disabled" href="#">
+            Disabled
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+}
